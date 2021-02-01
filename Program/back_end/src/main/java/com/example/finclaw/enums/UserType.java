bb6be461e84a294c328a83ad34406719f416bffd
plutooooooo <181250082@smail.nju.edu.cn>
2021-02-01 23:00:07 +0800

    backend

--- Program/back_end/src/main/java/com/example/finclaw/enums/UserType.java
+++ Program/back_end/src/main/java/com/example/finclaw/enums/UserType.java
@@ -2,9 +2,11 @@ package com.example.finclaw.enums;
 
 
 public enum UserType {
-    Manager("1"),
-    DataManager("2"),
-    Cooperation("3");
+    Bank("1"),
+    Company("2"),//小微企业
+    DataProvider("3"),
+    Admin("4"),//平台管理员
+    Unauthorized("5");//未认证用户
     private final String value;
 
     UserType(String value) {
