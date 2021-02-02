9e0f55afda1b5047aa5a0edc933e8b9719ca1a00
UKULELER <181250010@smail.nju.edu.cn>
2021-02-02 22:10:31 +0800

    feat: sql

--- Program/back_end/sql/FinClaw.sql
+++ Program/back_end/sql/FinClaw.sql
@@ -1,7 +1,7 @@
 
 -- MySQL dump 10.13  Distrib 5.7.19, for macos10.12 (x86_64)
 --
--- Host: 127.0.0.1    Database: Hotel
+-- Host: 127.0.0.1    Database: FinClaw
 -- ------------------------------------------------------
 -- Server version	5.7.19-log
 
@@ -30,8 +30,6 @@ CREATE TABLE `User` (
     `username` varchar(255) NOT NULL,
     `phoneNumber` varchar(255) NOT NULL,
     `role` varchar(255) NOT NULL,
-    `cooperation` varchar(255),
-    `cooperationID` varchar(255),
     PRIMARY KEY (`userID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -42,131 +40,17 @@ CREATE TABLE `User` (
 
 BEGIN;
 /*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES (1, '123@qq.com', '123456', 'root', 12345678911, 'Manager','中国银行', null);
-INSERT INTO `User` VALUES (2, '123456@qq.com', '123456', 'root1', 12345678911, 'DataManager','中国银行', null);
-INSERT INTO `User` VALUES (3, 'ali@qq.com', '123456', 'ALi', 12345678911, 'Cooperation','阿里巴巴', 1);
-INSERT INTO `User` VALUES (4, 'jd@qq.com', '123456', 'JD', 12345678911, 'Cooperation','京东', 2);
-INSERT INTO `User` VALUES (5, 'mt@qq.com', '123456', 'MT', 12345678911, 'Cooperation','美团', 3);
-INSERT INTO `User` VALUES (6, 'bd@qq.com', '123456', 'BD', 12345678911, 'Cooperation','字节跳动', 4);
+INSERT INTO `User` VALUES (1, '123@qq.com', '123456', 'root', 12345678911, 'Admin');
+INSERT INTO `User` VALUES (2, '123456@qq.com', '123456', 'root1', 12345678911, 'Admin');
+INSERT INTO `User` VALUES (3, 'huaqi@qq.com', '123456', '花旗银行', 12345678911, 'Bank');
+INSERT INTO `User` VALUES (4, 'jd@qq.com', '123456', 'JD', 12345678911, 'DataProvider');
+INSERT INTO `User` VALUES (5, 'mt@qq.com', '123456', 'MT', 12345678911, 'DataProvider');
+INSERT INTO `User` VALUES (6, 'bd@qq.com', '123456', 'BD', 12345678911, 'DataProvider');
+INSERT INTO `User` VALUES (7, 'a@qq.com', '123456', 'A', 12345678911, 'Company');
+INSERT INTO `User` VALUES (8, 'b@qq.com', '123456', 'B', 12345678911, 'Unauthorized');
 /*!40000 ALTER TABLE `User` ENABLE KEYS */;
 COMMIT;
 
---
--- Table structure for table `File`
---
-
-DROP TABLE IF EXISTS `File`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `File` (
-    `fileID` int(11) NOT NULL AUTO_INCREMENT,
-    `fileName` varchar(255) NOT NULL,
-    `uploadTime` timestamp DEFAULT current_timestamp,
-    `operatorID` int(11) NOT NULL,
-    `projectID` int(11) NOT NULL,
-    `trainedTimes` int(11) DEFAULT 0,
-    `filePath` varchar(255) NOT NULL,
-    PRIMARY KEY (`fileID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `File`
---
-
-BEGIN;
-/*!40000 ALTER TABLE `File` DISABLE KEYS */;
-INSERT INTO `File` VALUES (1, 'test.csv' ,NOW() ,1 ,1  ,0,'D:/user');
-/*!40000 ALTER TABLE `File` ENABLE KEYS */;
-COMMIT;
-
-
---
--- Table structure for table `Monitor`
---
-
-DROP TABLE IF EXISTS `Monitor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Monitor` (
-   `monitorID` int(11) NOT NULL AUTO_INCREMENT,
-   `userID` int(11) NOT NULL,
-   `projectID` int(11) NOT NULL,
-   `operateTime` timestamp default current_timestamp,
-   `operation` varchar(255) NOT NULL,
-    primary key (`monitorID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Monitor`
---
-
-BEGIN;
-/*!40000 ALTER TABLE `Monitor` DISABLE KEYS */;
-INSERT INTO `Monitor` VALUES (1, 1, 1, NOW(), 'Add');
-/*!40000 ALTER TABLE `Monitor` ENABLE KEYS */;
-COMMIT;
-
-
---
--- Table structure for table `Cooperation`
---
-
-DROP TABLE IF EXISTS `Cooperation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Cooperation` (
-    `cooperationID` int(11) NOT NULL AUTO_INCREMENT,
-    `cooperationName` varchar(255) NOT NULL,
-    `phoneNumber` varchar(255) NOT NULL,
-    `ipAddress` varchar(255),
-    `serviceName` varchar(255),
-    `servicePassword` varchar(255),
-    PRIMARY KEY (`cooperationID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Cooperation`
---
-
-BEGIN;
-/*!40000 ALTER TABLE `Cooperation` DISABLE KEYS */;
-INSERT INTO `Cooperation` VALUES (1, '阿里巴巴', 12345678911, null, null, null);
-INSERT INTO `Cooperation` VALUES (2, '京东', 12345678911, null, null, null);
-INSERT INTO `Cooperation` VALUES (3, '美团', 12345678911, null, null, null);
-INSERT INTO `Cooperation` VALUES (4, '字节跳动', 12345678911, null, null, null);
-
-/*!40000 ALTER TABLE `Cooperation` ENABLE KEYS */;
-COMMIT;
-
---
--- Table structure for table `Model`
---
-
-DROP TABLE IF EXISTS `Model`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Model` (
-    `modelID` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    PRIMARY KEY (`modelID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Model`
---
-
-BEGIN;
-/*!40000 ALTER TABLE `Model` DISABLE KEYS */;
-INSERT INTO `Model` VALUES (1, 'SVM');
-INSERT INTO `Model` VALUES (2, 'Regression');
-INSERT INTO `Model` VALUES (3, 'NeuralNetwork');
-INSERT INTO `Model` VALUES (4, 'SecureBoost');
-/*!40000 ALTER TABLE `Model` ENABLE KEYS */;
-COMMIT;
 
 --
 -- Table structure for table `Project`
@@ -176,16 +60,15 @@ DROP TABLE IF EXISTS `Project`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Project` (
-    `projectID` int(11) NOT NULL AUTO_INCREMENT,
-    `projectName` varchar(255) NOT NULL,
-    `description` varchar(255),
-    `startTime` timestamp,
-    `endTime` timestamp,
-    `status` varchar(255),
-    `fileID` int(11),
-    `modelID` varchar(255),
-    `modelVersion` varchar(255),
-    PRIMARY KEY (`projectID`)
+                           `projectID` int(11) NOT NULL AUTO_INCREMENT,
+                           `userID` int(11) NOT NULL,
+                           `projectName` varchar(255) NOT NULL,
+                           `description` varchar(255),
+                           `startTime` timestamp,
+                           `endTime` timestamp,
+                           `status` varchar(255),
+                           `modelID` varchar(255),
+                           PRIMARY KEY (`projectID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -195,39 +78,161 @@ CREATE TABLE `Project` (
 
 BEGIN;
 /*!40000 ALTER TABLE `Project` DISABLE KEYS */;
-INSERT INTO `Project`(projectID, projectName, description, startTime, endTime, status) VALUES (1, '南京市活动1', '企业信用预测', NOW(), date_add(NOW(), interval 10 day), 'Running');
-INSERT INTO `Project`(projectID, projectName, description, startTime, endTime, status) VALUES (2, '南京市活动2', '企业信用预测', NOW(), date_add(NOW(), interval 1 month), 'Running');
-INSERT INTO `Project`(projectID, projectName, description, startTime, endTime, status) VALUES (3, '南京市活动2', '企业信用预测', NOW(), date_add(NOW(), interval 2 month), 'Running');
+INSERT INTO `Project` VALUES (1, 3, '南京市活动1', '企业信用预测', NOW(), date_add(NOW(), interval 10 day), 'Running',null);
+INSERT INTO `Project` VALUES (2, 3, '南京市活动2', '企业信用预测', NOW(), date_add(NOW(), interval 1 month), 'Running',null);
+INSERT INTO `Project` VALUES (3, 3, '南京市活动2', '企业信用预测', NOW(), date_add(NOW(), interval 2 month), 'Running',null);
 /*!40000 ALTER TABLE `Project` ENABLE KEYS */;
 COMMIT;
 
+
 --
--- Table structure for table `Attend`
+-- Table structure for table `Attendance`
 --
 
-DROP TABLE IF EXISTS `Attend`;
+DROP TABLE IF EXISTS `Attendance`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Attend` (
-    `cooperationID` int(11) NOT NULL,
-    `projectID` int(11) NOT NULL,
-    `ready` int(11) NOT NULL,
-    `authority` int(11) NOT NULL,
-    `filepath` varchar(255)
+CREATE TABLE `Attendance` (
+                              `userID` int(11) NOT NULL,
+                              `projectID` int(11) NOT NULL,
+                              `isChosen` int(11) NOT NULL,
+                              `isReady` int(11) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Attend`
+-- Dumping data for table `Attendance`
 --
 
 BEGIN;
-/*!40000 ALTER TABLE `Attend` DISABLE KEYS */;
-INSERT INTO `Attend` VALUES (1, 1, 0, 1, null);
-INSERT INTO `Attend` VALUES (2, 1, 0, 1, null);
-INSERT INTO `Attend` VALUES (3, 1, 0, 1, null);
-INSERT INTO `Attend` VALUES (4, 1, 0, 1, null);
-/*!40000 ALTER TABLE `Attend` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Attendance` DISABLE KEYS */;
+INSERT INTO `Attendance` VALUES (4, 1, 1, 1);
+INSERT INTO `Attendance` VALUES (5, 1, 1, 1);
+INSERT INTO `Attendance` VALUES (6, 1, 1, 1);
+/*!40000 ALTER TABLE `Attendance` ENABLE KEYS */;
+COMMIT;
+
+
+--
+-- Table structure for table `Model`
+--
+
+DROP TABLE IF EXISTS `Model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Model` (
+                         `modelID` int(11) NOT NULL AUTO_INCREMENT,
+                         `mid` varchar(255) NOT NULL,
+                         `modelVersion` varchar(255) NOT NULL,
+                         `modelName` varchar(255) NOT NULL,
+                         PRIMARY KEY (`modelID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Model`
+--
+
+BEGIN;
+/*!40000 ALTER TABLE `Model` DISABLE KEYS */;
+INSERT INTO `Model` VALUES (1,'null','null', 'SVM');
+INSERT INTO `Model` VALUES (2,'null','null', 'Regression');
+INSERT INTO `Model` VALUES (3,'null','null', 'NeuralNetwork');
+INSERT INTO `Model` VALUES (4,'null','null', 'SecureBoost');
+/*!40000 ALTER TABLE `Model` ENABLE KEYS */;
+COMMIT;
+
+
+--
+-- Table structure for table `lendingHistory`
+--
+
+DROP TABLE IF EXISTS `lendingHistory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lendingHistory` (
+    `lendingHistoryID` int(11) NOT NULL AUTO_INCREMENT,
+    `bankID` int(11) NOT NULL,
+    `lenderID` int(11) NOT NULL,
+    `bankName` varchar(255) NOT NULL,
+    `amount` DECIMAL NOT NULL,
+    `interestRate` DECIMAL NOT NULL,
+    `phoneNumber` varchar(255) NOT NULL,
+    `hasDealt` int(11) NOT NULL,
+    PRIMARY KEY (`lendingHistoryID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lendingHistory`
+--
+
+BEGIN;
+/*!40000 ALTER TABLE `lendingHistory` DISABLE KEYS */;
+INSERT INTO `lendingHistory` VALUES (1, 3, 7, '花旗银行', 100000, 0.1, 12345678911, 0);
+/*!40000 ALTER TABLE `lendingHistory` ENABLE KEYS */;
+COMMIT;
+
+
+--
+-- Table structure for table `serverInfo`
+--
+
+DROP TABLE IF EXISTS `serverInfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `serverInfo` (
+                              `userID` int(11) NOT NULL,
+                              `projectID` int(11) NOT NULL,
+                              `ipAddress` varchar(255) NOT NULL,
+                              `serverName` varchar(255) NOT NULL,
+                              `serverPassword` varchar(255) NOT NULL,
+                              `filePath` varchar(255) NOT NULL,
+                              PRIMARY KEY (`userId`,`projectID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `serverInfo`
+--
+
+BEGIN;
+/*!40000 ALTER TABLE `serverInfo` DISABLE KEYS */;
+INSERT INTO `serverInfo` VALUES (4, 1, 'null', 'null', 'null', 'null');
+INSERT INTO `serverInfo` VALUES (5, 1, 'null', 'null', 'null', 'null');
+INSERT INTO `serverInfo` VALUES (6, 1, 'null', 'null', 'null', 'null');
+/*!40000 ALTER TABLE `serverInfo` ENABLE KEYS */;
+COMMIT;
+
+
+--
+-- Table structure for table `loanApplication`
+--
+
+DROP TABLE IF EXISTS `loanApplication`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `loanApplication` (
+                                   `loanApplicationID` int(11) NOT NULL AUTO_INCREMENT,
+                                   `userID` int(11) NOT NULL,
+                                   `amount` DECIMAL NOT NULL,
+                                   `certificationCode` varchar(255) NOT NULL,
+                                   `registrationCode` varchar(255) NOT NULL,
+                                   `companyName` varchar(255) NOT NULL,
+                                   `phoneNumber` varchar(255) NOT NULL,
+                                   `hasDealt` int(11) NOT NULL,
+                                   PRIMARY KEY (`loanApplicationID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loanApplication`
+--
+
+BEGIN;
+/*!40000 ALTER TABLE `loanApplication` DISABLE KEYS */;
+INSERT INTO `loanApplication` VALUES (1, 7, 100000, 'null', 'null', 'A', 12345678911, 0);
+/*!40000 ALTER TABLE `loanApplication` ENABLE KEYS */;
 COMMIT;
 
 
