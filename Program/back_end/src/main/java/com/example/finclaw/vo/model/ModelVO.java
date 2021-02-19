34fc4c34472c737befc9f4a192c446c5614cb90d
UKULELER <181250010@smail.nju.edu.cn>
2021-02-19 23:05:49 +0800

    fix: ModelVO

--- Program/back_end/src/main/java/com/example/finclaw/vo/model/ModelVO.java
+++ Program/back_end/src/main/java/com/example/finclaw/vo/model/ModelVO.java
@@ -4,11 +4,19 @@ import com.example.finclaw.po.Model;
 
 public class ModelVO {
     private Integer modelID;
+    private String mid;
+    private String modelVersion;
     private String modelName;
+    private String jid;
+
+    public ModelVO(){}
 
     public ModelVO(Model model) {
         this.modelID = model.getModelID();
+        this.mid = model.getMid();
+        this.modelVersion = model.getModelVerion();
         this.modelName = model.getModelName();
+        this.jid = model.getJid();
     }
 
     public Integer getModelID() {
@@ -25,5 +33,29 @@ public class ModelVO {
 
     public void setModelName(String modelName) {
         this.modelName = modelName;
+    }
+
+    public String getMid() {
+        return mid;
+    }
+
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+
+    public String getModelVersion() {
+        return modelVersion;
+    }
+
+    public void setModelVersion(String modelVersion) {
+        this.modelVersion = modelVersion;
+    }
+
+    public String getJid() {
+        return jid;
+    }
+
+    public void setJid(String jid) {
+        this.jid = jid;
     }
 }
